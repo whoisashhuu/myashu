c524941a2446fefc306982093ed102a932a2492a
whoisashhuu <ashuumuz95@gmail.com>
2025-06-11 21:50:21 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+// Script for adding interactivity
+document.querySelector('.contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Message Sent Successfully!');
+});
+a
